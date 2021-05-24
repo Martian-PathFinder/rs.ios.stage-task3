b7bd1847876338af3b50070198b4d356fcbcfb32
Oleksii Sur <oleksii_sur@epam.com>
2021-05-24 18:09:43 +0300

    feat: DateHelperTests has complited.

--- Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
@@ -5,25 +5,50 @@
 #pragma mark - First
 
 -(NSString *)monthNameBy:(NSUInteger)monthNumber {
-    return nil;
+    NSArray *array = @[@"January", @"February", @"March", @"April", @"May", @"June", @"July", @"August", @"September", @"October", @"November", @"December"];
+    
+    if (monthNumber < 1 || monthNumber > 12) return nil;
+    return array[monthNumber - 1];
 }
 
 #pragma mark - Second
 
 - (long)dayFromDate:(NSString *)date {
-    return 0;
+    NSDateFormatter *formatter = [NSDateFormatter new];
+    formatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ss'Z'";
+    NSDate *myDate = [formatter dateFromString:date];
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSInteger day = [calendar component:NSCalendarUnitDay fromDate: myDate];
+    return day;
 }
 
 #pragma mark - Third
 
 - (NSString *)getDayName:(NSDate*) date {
-    return nil;
+    NSArray *dateWeek = @[@"Пн", @"Вт", @"Ср", @"Чт", @"Пт", @"Сб", @"Вс"];
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"EEEE"];
+    
+    NSCalendar *gregorian = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    NSDateComponents *comps = [gregorian components: NSCalendarUnitWeekday fromDate: date];
+    NSInteger weekday = [comps weekday];
+    
+    if (weekday < 1 || weekday > 7) return nil;
+    
+    return dateWeek[weekday - 2];
 }
 
 #pragma mark - Fourth
 
 - (BOOL)isDateInThisWeek:(NSDate *)date {
-    return NO;
+    NSCalendar *calendar= [NSCalendar currentCalendar];
+    NSInteger curWeek = [calendar component: NSCalendarUnitWeekOfMonth fromDate: [NSDate now]];
+    NSInteger myWeek = [calendar component: NSCalendarUnitWeekOfMonth fromDate: date];
+    if (curWeek == myWeek) {
+        return YES;
+    } else {
+        return NO;
+    }
 }
 
 @end
