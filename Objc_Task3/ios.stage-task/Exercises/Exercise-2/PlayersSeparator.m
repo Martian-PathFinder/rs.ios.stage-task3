6a991870118d8df894a983d1c39eb05bb6e76fcb
Oleksii Sur <oleksii_sur@epam.com>
2021-05-24 20:07:01 +0300

    feat: task playerSeporator was completed.

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,30 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    NSInteger player1, player2, player3;
+    NSInteger teams = 0;
+    
+    for (int i = 0; i < ratingArray.count; i++) {
+      player1 = [ratingArray[i] integerValue];
+        for (int j = i + 1; j < ratingArray.count; j++) {
+            if ( [ratingArray[i] integerValue] < [ratingArray[j] integerValue] || [ratingArray[i] integerValue] > [ratingArray[j] integerValue]) {
+                player2 = [ratingArray[j] integerValue];
+                for (int k = j + 1; k < ratingArray.count; k++) {
+                    if (( ([ratingArray[j] integerValue] < [ratingArray[k] integerValue]) &&
+                         ([ratingArray[i] integerValue] < [ratingArray[j] integerValue]) ) ||
+                        ( ([ratingArray[j] integerValue] > [ratingArray[k] integerValue]) &&
+                         ([ratingArray[i] integerValue] > [ratingArray[j] integerValue]) )) {
+                        
+                        player3 = [ratingArray[k] integerValue];
+                        teams++;
+                    }
+                }
+            }
+        }
+        
+    }
+    
+    return teams;
 }
 
 @end
